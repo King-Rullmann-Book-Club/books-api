4b6bb9ab90166c25bf52c0d01d685caa63196c62
Scott King <me@kingscott.ca>
2024-07-16 11:01:03 -0400

    Update comment in endpoints.go

--- pkg/v1/endpoints/books/endpoints.go
+++ pkg/v1/endpoints/books/endpoints.go
@@ -6,7 +6,21 @@ import (
     "github.com/go-kit/kit/endpoint"
 )
 
-// Endpoints represents available endpoints for the book service. 
+// COPIED FROM GO-KIT EXAMPLES:
+// Endpoints collects all of the endpoints that compose a profile service. It's
+// meant to be used as a helper struct, to collect all of the endpoints into a
+// single parameter.
+//
+// In a server, it's useful for functions that need to operate on a per-endpoint
+// basis. For example, you might pass an Endpoints to a function that produces
+// an http.Handler, with each method (endpoint) wired up to a specific path. (It
+// is probably a mistake in design to invoke the Service methods on the
+// Endpoints struct in a server.)
+//
+// In a client, it's useful to collect individually constructed endpoints into a
+// single type that implements the Service interface. For example, you might
+// construct individual endpoints using transport/http.NewClient, combine them
+// into an Endpoints, and return it to the caller as a Service.
 type Endpoints struct {
     GetBook endpoint.Endpoint
 }
